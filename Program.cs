5b5602de9593fbb8a4bf924e9c378856b79f5329
sujith-sairam <saisujith589@gmail.com>
2023-06-20 12:50:31 +0530

    Authentication with Jwt

--- Program.cs
+++ Program.cs
@@ -1,11 +1,49 @@
+global using dotnet_rpg.Models;
+global using dotnet_rpg.Services.CharacterService;
+global using  dotnet_rpg.Dtos.Character;
+global using  AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using  dotnet_rpg.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=> {
+    c.AddSecurityDefinition("oauth2",new Microsoft.OpenApi.Models.OpenApiSecurityScheme{
+        Description = """ Standard Authorization header using the Bearer scheme. Example: "bearer {token}" """,
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped<ICharacterService,CharacterService>();
+builder.Services.AddScoped<IAuthRepository,AuthRepository>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                    .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value!)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+            };
+            
+        });
+
 
 var app = builder.Build();
 
@@ -17,6 +55,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ Services/CharacterService/CharacterService.cs
@@ -0,0 +1,107 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character { Name = "Sam" }
+        };
+        private readonly IMapper _mapper;
+
+        private readonly DataContext _context;
+         public CharacterService(IMapper mapper,DataContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+            
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            var character = _mapper.Map<Character>(newCharacter);
+
+            _context.Characters.Add(character);
+            await _context.SaveChangesAsync();
+            serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+              var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+
+            try {
+
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            if (character is null)
+                throw new Exception($"Character with id '{id}' not found");
+            
+            _context.Characters.Remove(character);
+            await _context.SaveChangesAsync();
+            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+        }
+        catch(Exception ex)
+        {
+            serviceResponse.Success = false;
+            serviceResponse.Message = ex.Message;
+        }
+            return serviceResponse;
+        }
+   
+
+        public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacter(int userId)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            var dbCharacters = await _context.Characters.Where(c => c.User!.Id == userId ).ToListAsync();
+            serviceResponse.Data = dbCharacters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
+        {
+            var serviceResponse = new ServiceResponse<GetCharacterDto>();
+            var dbCharacter =  await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
+            return serviceResponse;
+        }
+
+        public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
+        {
+             var serviceResponse = new ServiceResponse<GetCharacterDto>();
+
+            try {
+
+            var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+            if (character is null)
+                throw new Exception($"Character with id '{updatedCharacter.Id}' not found");
+            
+            
+            character.Name = updatedCharacter.Name;
+            character.Hitpoints = updatedCharacter.Hitpoints;
+            character.Strength = updatedCharacter.Strength;
+            character.Defence = updatedCharacter.Defence;
+            character.Intelligence = updatedCharacter.Intelligence;
+            character.Class = updatedCharacter.Class;
+
+            await _context.SaveChangesAsync();
+            serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+
+            return serviceResponse;
+        }
+        catch(Exception ex)
+        {
+            serviceResponse.Success = false;
+            serviceResponse.Message = ex.Message;
+        }
+            return serviceResponse;
+        }
+    }
+}
